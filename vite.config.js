aab1feeb30ac49b7b85e6c1306397b6e976ea424
Willie Sinclair <ws1985media@gmail.com>
2024-11-18 14:13:45 -0500

    update
    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/se_project_react/",
   plugins: [react()],
-  // add the server object
   server: {
     port: 3000,
-  },
-});
+  }
+})
