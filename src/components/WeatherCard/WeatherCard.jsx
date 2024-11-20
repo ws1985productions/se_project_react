8cd6d47a308cf0435667c68fe2ec320f3c6533a1
Willie Sinclair <ws1985media@gmail.com>
2024-11-20 13:53:16 -0500

    Updated per review
    Added Weather symbol to weather card

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -22,7 +22,7 @@ function WeatherCard({ weatherData }) {
 
   return (
     <section className="weather-card">
-      <p className="weather-card__temp">{weatherData.temp.F} &deg;</p>
+      <p className="weather-card__temp">{weatherData.temp.F} &deg; F </p>
       <img
         src={weatherOption?.url}
         alt={`Card showing ${weatherOption?.day ? "day" : "night"}time ${
