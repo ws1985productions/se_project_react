b7af54e2a0aa2508e9506699683004b5f597bde6
Willie Sinclair <ws1985media@gmail.com>
2024-11-30 15:09:14 -0500

    Render Correction
    Correction to Main.jsx and WeatherAPI.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./components/App/App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import App from './components/App/App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-    <App/>
+      <App />
     </HashRouter>
-  </React.StrictMode>
-);
-
+  </React.StrictMode>,
+)
--- src/utils/weatherAPI.js
+++ src/utils/weatherAPI.js
@@ -1,41 +1,41 @@
-export const getWeather = ({ latitude, longitude }, APIkey) => {
-  return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
-  ).then((res) => {
-    if (res.ok) {
-      return res.json();
-    } else {
-      return Promise.reject(`Error: ${res.status}`);
-    }
-  });
 
-  {const toCelsius = (temp) => {
-    const cel = Math.round((temp - 32) * 5/9);
-    return cel;
-}}
-};
+export const getWeather = ({latitude, longitude}, APIkey) => {
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`)
+   .then((res) => {
+       if (res.ok) {
+           return res.json();
+       } else {
+           return Promise.reject(`Error: ${res.status}`);
+       }
+   });
+}
+
+const toCelsius = (temp) => {
+   const cel = Math.round((temp - 32) * 5/9);
+   return cel;
+}
 
 export const filterWeatherData = (data) => {
-  const result = {};
-  result.city = data.name;
-  result.temp = { F: Math.round (data.main.temp), C: toCelsius (Math.round(data.main.temp)) };
-  result.type = getWeatherType(result.temp.F);
-  result.condition = data.weather[0].main.toLowerCase();
-  result.isDay = isDay(data.sys, Date.now());
-  return result;
-};
+   const result = {};
+   result.city = data.name;
+   result.temp = { F: Math.round(data.main.temp), C: toCelsius(Math.round(data.main.temp))};
+   result.type = getWeatherType(result.temp.F);
+   result.condition = data.weather[0].main.toLowerCase();
+   result.isDay = isDay(data.sys, Date.now());
+   return result;
+}
 
-const isDay = ({ sunrise, sunset }, now) => {
-  return sunrise * 1000 < now && now < sunset * 1000;
-};
+const isDay = ({sunrise, sunset}, now) => {
+   return sunrise * 1000 < now && now < sunset * 1000;
+} 
 
 const getWeatherType = (temperature) => {
-  temperature = Math.round(temperature);
-  if (temperature >= 86) {
-    return "hot";
-  } else if (temperature >= 66) {
-    return "warm";
-  } else {
-    return "cold";
-  }
-};
+   temperature = Math.round(temperature);
+   if (temperature >= 86) {
+       return 'hot';
+     } else if (temperature >= 66) {
+       return 'warm';
+     } else {
+       return 'cold';
+     }
+}
