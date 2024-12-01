35cb4f5619a527969492b90ef1f8ea474da07bfb
Willie Sinclair <ws1985media@gmail.com>
2024-12-01 16:24:15 -0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3001';
+const baseUrl = 'http://localhost:3000';
 
 function checkRes(res) {
     return res.ok ? res.json() : Promise.reject(`Error: ${res.status}`)
