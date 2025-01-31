77a4b7166e96576ce0fa1ed13f148064cbeccdbd
Willie Sinclair <ws1985media@gmail.com>
2025-01-31 14:38:41 -0500

    updated add pictures
    added images and removal of files that are not needed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,12 @@
-import './Footer.css'
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <footer className="footer">
-            <div className="footer__content">
-                <p className='footer__content_name'>Developed by Willie Sinclair</p>
-                <p className='footer__content_year'>2024</p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <p className="footer__name">Developed by Willie Sinclair</p>
+      <p className="footer__year">2024</p>
+    </footer>
+  );
 }
 
 export default Footer;
