08abaac4ddbb09f9253009e47428af3fb96de14a
Willie Sinclair <ws1985media@gmail.com>
2025-02-03 13:32:20 -0500

    Update RegisterModal.jsx

--- src/components/RegisterModal/RegisterModal.jsx
+++ src/components/RegisterModal/RegisterModal.jsx
@@ -1,106 +1,120 @@
-import "./RegisterModal.css";
-import { useState } from "react";
+import React, { useState } from "react";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import "./RegisterModal.css";
 
 const RegisterModal = ({
-    closeActiveModal,
-    activeModal,
-    handleRegistration,
-    onLoginClick,
+  handleCloseModal,
+  onRegister,
+  isOpen,
+  onLoginClick,
 }) => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [avatar, setAvatar] = useState("");
-    const handleNameChange = (e) => {
-        console.log(e.target.value);
-        setName(e.target.value);
-    };
-    const handlePasswordChange = (e) => {
-        console.log(e.target.value);
-        setPassword(e.target.value);
-    };
-    const handleEmailChange = (e) => {
-        console.log(e.target.value);
-        setEmail(e.target.value);
-    };
-    const handleAvatarChange = (e) => {
-        console.log(e.target.value);
-        setAvatar(e.target.value);
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        handleRegistration(name, email, password, avatar);
-    };
+  const [name, setName] = useState(""); // Not sure if this variable will need to be changed.
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
 
-    return (
-        <ModalWithForm
-        title="Sign up"
-        isOpen={activeModal === "signup"}
-        onClose={closeActiveModal}
-        onSubmit={handleSubmit}
+  const [email, setEmail] = useState("");
+  const handleEmailChange = (e) => {
+    // Adapt for email-username
+    setEmail(e.target.value);
+  };
+
+  const [password, setPassword] = useState(""); // Adapt for password
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const [avatar, setAvatar] = useState("");
+  const handleAvatarChange = (e) => {
+    setAvatar(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onRegister({ name, avatar, email, password });
+  };
+
+  return (
+    <ModalWithForm
+      title={"Sign Up"}
+      onClose={handleCloseModal}
+      modalType={"register"}
+      buttonText={"Sign Up"}
+      isOpen={isOpen}
+      onSubmit={handleSubmit}
+      extraContent={
+        <span
+          onClick={() => {
+            onLoginClick();
+          }}
+          className="modal__or_login"
         >
-            <label>
-                Email *
-                <input
-                className="modal__input"
-                type="email"
-                name="email"
-                minLength="1"
-                maxLength="30"
-                placeholder="Email"
-                value={email}
-                onChange={handleEmailChange}
-                />
-            </label>
-            <label>
-                Password *
-                <input
-                className="modal__input"
-                type="password"
-                name="password"
-                minLength="1"
-                placeholder="Password"
-                value={password}
-                onChange={handlePasswordChange}
-                />
-            </label>
-            <label>
-                Name * 
-                <input 
-                className ="modal__input"
-                type="text"
-                name="Name"
-                minLength="1"
-                maxLength="30"
-                placeholder="Name"
-                value={name}
-                onChange={handleNameChange}
-                />
-            </label>
-            <label>
-                Avatar URL *
-                <input
-                className="modal__input"
-                type="url"
-                name="avatar"
-                placeholder="Avatar URL"
-                value={avatar}
-                onChange={handleAvatarChange}
-                />
-            </label>
-            <div className="modal__button-div">
-            <button type="submit" className="modal__submit">
-                Sign Up
-                {" "}
-            </button>
-            <button type="button" className="modal__button" onClick={onLoginClick}>
-                {" "}
-                or Log In
-            </button>
-            </div>
-        </ModalWithForm>
-    );
+          or Log in
+        </span>
+      }
+    >
+      <div className="modal__input_wrapper">
+        <label className="modal__label_input">
+          Email*{" "}
+          <input
+            type="email"
+            name="email"
+            maxLength="100"
+            placeholder="Email"
+            className="modal__input"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </label>
+      </div>
+      <div className="modal__input_wrapper">
+        <label className="modal__label_input">
+          Password*{" "}
+          <input
+            type="password"
+            name="password"
+            minLength="8"
+            maxLength="100"
+            placeholder="Password"
+            className="modal__input"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+      </div>
+      <div className="modal__input_wrapper">
+        <label className="modal__label_input">
+          Name *{" "}
+          <input
+            type="text"
+            name="name"
+            minLength="1"
+            maxLength="100"
+            placeholder="Name"
+            className="modal__input"
+            value={name}
+            onChange={handleNameChange}
+          />
+        </label>
+      </div>
+      <div className="modal__input_wrapper">
+        <label className="modal__label_input">
+          Avatar URL *{" "}
+          <input
+            type="url"
+            name="avatar"
+            minLength="5"
+            maxLength="100"
+            placeholder="Avatar URL"
+            className="modal__input"
+            value={avatar}
+            onChange={handleAvatarChange}
+          />
+        </label>
+      </div>
+    </ModalWithForm>
+  );
 };
 
 export default RegisterModal;
